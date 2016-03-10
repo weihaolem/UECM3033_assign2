a4345097151c11fc46c99b0bd7f10286a9fc0580
LEM WEI HAO <lukazala01@1utar.my>
2016-03-11 01:20:15 +0800

    full work upload

--- task1.py
+++ task1.py
@@ -3,39 +3,84 @@ import numpy as np
 #You can import some modules or create additional functions
 
 def lu(A, b):
-    sol = []
-    # Edit here to implement your code
+    sol=[]
+    n = len(A)
+    for k in range(0, n-1):
+        for i in range(k+1, n):
+            if A[i,k] != 0.0:
+                lam = A[i,k] / A[k,k]
+                A[i, k+1:n] = A[i, k+1:n] - lam * A[k, k+1:n]
+                A[i, k] = lam
+    for k in range(1,n):
+        b[k] = b[k] - np.dot(A[k,0:k], b[0:k])
+    b[n-1]=b[n-1]/A[n-1, n-1]
+    for k in range(n-2, -1, -1):
+        b[k] = (b[k] - np.dot(A[k,k+1:n], b[k+1:n]))/A[k,k]
+    sol=b
     return list(sol)
 
-def sor(A, b):
+def sor(A, b,tol):
     sol = []
-    # Edit here to implement your code
+    xOld = np.empty_like(b)
+    error = 1e12
+
+    D = np.diag(np.diag(A))
+    L = np.tril(A, -1)
+    U = A - L - D
+
+    Mj = np.dot(np.linalg.inv(D), -(L+U))
+    rho_Mj = max(abs(np.linalg.eigvals(Mj)))
+    w = 2/(1+np.sqrt(1-rho_Mj**2))
+
+    T = np.linalg.inv(D+w*L)
+    Lw = np.dot(T, -w*U+(1-w)*D)
+    c = np.dot(T, w*b)
+
+    while error > tol:
+        x = np.dot(Lw, xOld) + c
+        error = np.linalg.norm(x-xOld)/np.linalg.norm(x)
+        xOld = x
+    sol = x
     return list(sol)
 
 def solve(A, b):
-    condition = True # State and implement your condition here
-    if condition:
+    # Spectral Radius Theorem stated, rho(x) < 1.This must satisfy so that SOR method
+    # converge for any x^(0), and the optimal relaxation parameter, w can be
+    #define in 1 < w < 2.
+    D = np.diag(np.diag(A))
+    L = np.tril(A, -1)
+    U = A - L - D
+    Mj = np.dot(np.linalg.inv(D), -(L+U))
+    rho_Mj = max(abs(np.linalg.eigvals(Mj)))
+    if rho_Mj < 1 :
+        condition = True
+    else:
+        condition = False
+    
+    #the method to choose due to the condition
+    if condition == True:
+        print('Solve by sor(A,b)')
+        return sor(A,b,1e-9)
+        
+    else:
         print('Solve by lu(A,b)')
         return lu(A,b)
-    else:
-        print('Solve by sor(A,b)')
-        return sor(A,b)
 
 if __name__ == "__main__":
     ## import checker
     ## checker.test(lu, sor, solve)
 
-    A = [[2,1,6], [8,3,2], [1,5,1]]
-    b = [9, 13, 7]
+    A = np.array([[2.,1,6], [8,3,2], [1,5,1]])
+    b = np.array([9., 13, 7])
     sol = solve(A,b)
     print(sol)
     
-    A = [[6566, -5202, -4040, -5224, 1420, 6229],
+    A = np.array([[6566, -5202, -4040, -5224, 1420, 6229],
          [4104, 7449, -2518, -4588,-8841, 4040],
          [5266,-4008,6803, -4702, 1240, 5060],
          [-9306, 7213,5723, 7961, -1981,-8834],
          [-3782, 3840, 2464, -8389, 9781,-3334],
-         [-6903, 5610, 4306, 5548, -1380, 3539.]]
-    b = [ 17603,  -63286,   56563,  -26523.5, 103396.5, -27906]
+         [-6903, 5610, 4306, 5548, -1380, 3539.]])
+    b = np.array([ 17603,  -63286,   56563,  -26523.5, 103396.5, -27906])
     sol = solve(A,b)
     print(sol)
--- task2.py
+++ task2.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
-
-img=mpimg.imread('Lenna.tiff')
+#Load image file
+img=mpimg.imread('image.jpg')
 [r,g,b] = [img[:,:,i] for i in range(3)]
 
-
+#Show image
 fig = plt.figure(1)
 ax1 = fig.add_subplot(2,2,1)
 ax2 = fig.add_subplot(2,2,2)
@@ -17,3 +17,68 @@ ax3.imshow(g, cmap = 'Greens')
 ax4.imshow(b, cmap = 'Blues')
 plt.show()
 
+#SVD
+U_r, sigma_r, V_r = np.linalg.svd(r,full_matrices = True)
+U_g, sigma_g, V_g = np.linalg.svd(g,full_matrices = True)
+U_b, sigma_b, V_b = np.linalg.svd(b,full_matrices = True)
+n_sigma = np.count_nonzero(sigma_r)
+print("non zero element for sigma of r, g, b : ", n_sigma)
+num_data, dim = r.shape
+
+# Compress-Lower Resolution picture
+#red image
+sigma_r_30=np.zeros_like(sigma_r)
+sigma_r_30[:30] = sigma_r[:30]
+r_30 = np.matrix(U_r) * np.diag(sigma_r_30) * np.matrix(V_r[:num_data, :])
+
+#green image
+sigma_g_30=np.zeros_like(sigma_g)
+sigma_g_30[:30] = sigma_g[:30]
+g_30 = np.matrix(U_g) * np.diag(sigma_g_30) * np.matrix(V_g[:num_data, :])
+
+#blue image
+sigma_b_30=np.zeros_like(sigma_b)
+sigma_b_30[:30] = sigma_b[:30]
+b_30 = np.matrix(U_b) * np.diag(sigma_b_30) * np.matrix(V_b[:num_data, :])
+
+#Show red, green, blue image.
+fig = plt.figure(1)
+ax1 = fig.add_subplot(3,1,1)
+ax2 = fig.add_subplot(3,1,2)
+ax3 = fig.add_subplot(3,1,3)
+ax1.imshow(r_30, cmap = 'Reds')
+ax2.imshow(g_30, cmap = 'Greens')
+ax3.imshow(b_30, cmap = 'Blues')
+fig = plt.gcf()
+fig.set_size_inches(15,15)
+plt.suptitle('Lower Resolution, size=30')
+plt.show()
+
+# Compress-Higher Resolution picture
+#red image
+sigma_r_200=np.zeros_like(sigma_r)
+sigma_r_200[:200] = sigma_r[:200]
+r_200 = np.matrix(U_r) * np.diag(sigma_r_200) * np.matrix(V_r[:num_data, :])
+
+#green image
+sigma_g_200=np.zeros_like(sigma_g)
+sigma_g_200[:200] = sigma_g[:200]
+g_200 = np.matrix(U_g) * np.diag(sigma_g_200) * np.matrix(V_g[:num_data, :])
+
+#blue image
+sigma_b_200=np.zeros_like(sigma_b)
+sigma_b_200[:200] = sigma_b[:200]
+b_200 = np.matrix(U_b) * np.diag(sigma_b_200) * np.matrix(V_b[:num_data, :])
+
+#Show red, green, blue image.
+fig = plt.figure(1)
+ax1 = fig.add_subplot(3,1,1)
+ax2 = fig.add_subplot(3,1,2)
+ax3 = fig.add_subplot(3,1,3)
+ax1.imshow(r_200, cmap = 'Reds')
+ax2.imshow(g_200, cmap = 'Greens')
+ax3.imshow(b_200, cmap = 'Blues')
+fig = plt.gcf()
+fig.set_size_inches(15,15)
+plt.suptitle('Higher Resolution, size=200')
+plt.show()
